d85dac1bedc73765700902c90dac4bfda5ba55b9
Magnus Holm <judofyr@gmail.com>
2017-05-18 13:29:04 +0200

    Initial Compiler/Scope

---
+++ c1/compiler.rb
@@ -0,0 +1,104 @@
+require_relative 'scope'
+require_relative 'parse_utils'
+
+require 'pathname'
+
+class FryFile
+  attr_reader :scope, :path, :parse_result
+
+  def initialize(path, compiler)
+    @path = path
+    @included_files = []
+    @compiler = compiler
+
+    @is_parsed = false
+
+    @include_scope = IncludeScope.new(@compiler.root_scope)
+    @include_scope.included_files = @included_files
+    @scope = SymbolScope.new(@include_scope)
+  end
+
+  def dir
+    @dir ||= Pathname.new(@path) + ".."
+  end
+
+  def new_walker(idx = 0)
+    Parser::Walker.new(@parse_result, idx)
+  end
+
+  def parse
+    return if @is_parsed
+
+    input = Parser::Input.new(File.read(path))
+    @parse_result = Parser::Fry.toplevels.parse(input)
+    if !@parse_result.eos?
+      raise "Parsing failed #{path}"
+    end
+
+    w = new_walker
+    until w.done?
+      tag = w.tag
+      idx = w.idx
+
+      case tag.name
+      when :struct, :func
+        w.next
+        name = w.read_ident
+        @scope[name] = FrySymbol.new(name, idx, self)
+        end_name = :"#{tag.name}_end"
+        w.next until w.tag_name == end_name
+        w.next
+      when :include
+        w.next
+        included_path = w.read_string
+        full_path = dir + (included_path + ".fry")
+        @included_files << @compiler.file(full_path.to_s)
+      else
+        raise "Unknown tag: #{tag.name}"
+      end
+    end
+
+    @is_parsed = true
+    self
+  end
+end
+
+class FrySymbol
+  def initialize(name, idx, file)
+    @name = name
+    @idx = idx
+    @file = file
+    @type = @file.parse_result.tags[@idx].name
+  end
+
+  def new_walker
+    @file.new_walker(@idx)
+  end
+end
+
+class Compiler
+  attr_reader :root_scope
+
+  def initialize
+    @files = {}
+    @file_queue = []
+    @root_scope = SymbolScope.new
+  end
+
+  def file(path)
+    if @files.has_key?(path)
+      @files[path]
+    else
+      file = FryFile.new(path, self)
+      @file_queue << file
+      @files[path] = file
+    end
+  end
+
+  def process_files
+    while file = @file_queue.pop
+      file.parse
+    end
+  end
+end
+
---
+++ c1/scope.rb
@@ -0,0 +1,47 @@
+class Scope
+  attr_reader :parent
+
+  def initialize(parent = nil)
+    @parent = parent
+  end
+
+  def [](key)
+    scope = self
+    while scope
+      if sym = scope.lookup(key)
+        return sym
+      end
+      scope = scope.parent
+    end
+    raise KeyError, "could not find symbol: #{key}"
+  end
+end
+
+class SymbolScope < Scope
+  def initialize(parent = nil)
+    super
+    @symbols = {}
+  end
+
+  def []=(key, value)
+    @symbols[key] = value
+  end
+
+  def lookup(key)
+    @symbols[key]
+  end
+end
+
+class IncludeScope < Scope
+  attr_accessor :included_files
+
+  def lookup(key)
+    included_files.each do |file|
+      if sym = file.scope.lookup(key)
+        return sym
+      end
+    end
+    nil
+  end
+end
+
